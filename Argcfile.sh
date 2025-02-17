bc78c7511e07a462989c62479ba38448a619c951
Matthew Delaney <MattDelaney_948@protonmail.com>
2025-02-16 23:17:06 -0600

    Added install/uninstall commands to Argcfile

--- Argcfile.sh
+++ Argcfile.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
-# @meta require-tools pandoc
+# @meta require-tools pandoc,sudo,tar,install
 
 set -e
 
-# @cmd Generate docs and rmeove argc dependency
+INSTALL_PATH="/usr/local/bin/bbcp"
+INSTALL_MAN_PATH="/usr/local/share/man/man1/bbcp.1"
+
+# @cmd
 build() {
     if [ -d "build/" ]; then
         rm -rf ./build
@@ -18,6 +21,21 @@ package() {
     build
     cd build
     tar czf bbcp.tar.gz *
+}
+
+# @cmd
+install() {
+    build
+    echo "Installing. Password may be necessary"
+    sudo install -m 755 build/bbcp.sh "$INSTALL_PATH"
+    sudo install -m 644 build/bbcp.1 "$INSTALL_MAN_PATH"
+}
+
+# @cmd
+uninstall() {
+    echo "Uninstalling. Password may be necessary"
+    sudo rm "$INSTALL_PATH"
+    sudo rm "$INSTALL_MAN_PATH"
 }
 
 # See more details at https://github.com/sigoden/argc
